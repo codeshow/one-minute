4210b5826f7ff90640cea089c4d7966e33c11337
Bruna Ferreira <bferreira.slv@gmail.com>
2023-11-25 12:38:40 -0300

    code 001 ajustado

--- code/one-minute-001.py
+++ code/one-minute-001.py
@@ -3,8 +3,13 @@ inteiro = 105
 fracionario = 1.3
 logico = True
 
+tipo_str = type(texto)
+tipo_int = type(inteiro)
+tipo_float = type(fracionario)
+tipo_bool = type(logico)
+
 print("**Tipos de dados básicos em Python**\n")
-print(f"Texto: \t\t{texto}\t {type(texto)}")
-print(f"Inteiro: \t{inteiro}\t\t {type(inteiro)}")
-print(f"Fracionário: \t{fracionario}\t\t {type(fracionario)}")
-print(f"Lógico: \t{logico}\t\t {type(logico)}")
+print(f"Texto: \t\t{texto}\t {tipo_str}")
+print(f"Inteiro: \t{inteiro}\t\t {tipo_int}")
+print(f"Fracionário: \t{fracionario}\t\t {tipo_float}")
+print(f"Lógico: \t{logico}\t\t {tipo_bool}")

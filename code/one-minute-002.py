8fe9c274f3760f2cde0f1ea86e4a9e697101488d
Bruna Ferreira <bferreira.slv@gmail.com>
2023-12-01 22:51:31 -0300

    code 002

---
+++ code/one-minute-002.py
@@ -0,0 +1,19 @@
+soma = 1 + 1
+subtracao = 2 - 1
+multiplicacao = 3 * 2
+divisao = 4 / 2
+
+divisao_inteira = 17 // 3
+resto_divisao = 17 % 3
+potenciacao = 5 ** 2
+precedencia_operacao = 5 + 2 * 2
+
+
+print(f'''** Operações **
+{"Soma:":<24} 1 + 1 = {soma}
+{"Subtração:":24} 2 - 1 = {subtracao}
+{"Multiplicação:":24} 3 * 2 = {multiplicacao}
+{"Divisao:":24} 4 / 2 = {divisao}
+{"Divisao Inteira:":24} 17 // 3 = {divisao_inteira}
+{"Resto da Divisao:":24} 17 % 3 = {resto_divisao}
+{"Precedência de operação:":24} 5 + 2 * 2 = {precedencia_operacao}''')

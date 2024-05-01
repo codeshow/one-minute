efc66e51f1abc5afc08a282fe02a3daec3a688ca
Bruna Ferreira <bferreira.slv@gmail.com>
2024-05-01 13:46:07 -0300

    code 020

---
+++ code/one-minute-020.py
@@ -0,0 +1,9 @@
+def soma(primeiros, segundos):
+    resultado = primeiros + segundos
+    return resultado
+
+print("*** Soma de dois números ***")
+print(soma(2, 3))
+print(soma(5, 7))
+print(soma(10, 20))
+print(soma(100, 200))

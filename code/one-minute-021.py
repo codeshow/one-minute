02644e326a10f15d89a369f1f356831ba7abf303
Bruna Ferreira <bferreira.slv@gmail.com>
2024-06-26 23:31:39 -0300

    code 021

---
+++ code/one-minute-021.py
@@ -0,0 +1,11 @@
+def funcao():
+    local = "Função"
+    return local
+
+print(f'Retorno da função: {funcao()}\n')
+
+def procedimento():
+    local = "Procedimento"
+    print(local)
+
+print(f'Execução do procedimento: {procedimento()}')

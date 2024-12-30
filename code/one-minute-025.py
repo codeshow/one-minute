39207f0e00d49654e5e270e077dad82e8af4a4e2
Bugs <bferreira.slv@gmail.com>
2024-12-30 10:44:27 -0300

    code 25

---
+++ code/one-minute-025.py
@@ -0,0 +1,14 @@
+# Estrutura de dados: Pilhas LIFO
+
+pilha = ['a', 'b', 'c']
+pilha.append('d')
+pilha.append('e')
+
+print(f""" *** Pilhas ***
+pilha: {pilha}
+pilha.pop(): {pilha.pop()}
+pilha.pop(): {pilha.pop()}
+pilha.pop(): {pilha.pop()}
+pilha.pop(): {pilha.pop()}
+pilha: {pilha}
+""")

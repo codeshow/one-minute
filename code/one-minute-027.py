c32379356183dd11cfea6eb4fdd2c8ec2cea1641
Bugs <bferreira.slv@gmail.com>
2024-12-30 10:56:46 -0300

    code 27

---
+++ code/one-minute-027.py
@@ -0,0 +1,20 @@
+list_manual_10 = [
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+]
+list_manual_1000 = ...
+
+# List comprehensions
+list_comp = [numero for numero in range(16)]
+
+# Lista com for
+list_for = []
+for numero in range(16):
+    list_for.append(numero)
+
+
+print(f""" *** Listas ***
+Lista com 'list comp':
+{list_comp}
+Lista com 'for':
+{list_for}
+""")

2119acb697aeb18f72eefdae46baa56ccd35a22c
Bugs <bferreira.slv@gmail.com>
2024-12-31 10:29:42 -0300

    code 31

---
+++ code/one-minute-031.py
@@ -0,0 +1,16 @@
+# Função dir()
+nomes_lista = dir(list)
+nomes_str = dir(str)
+nomes_local = dir()
+
+
+print(f""" **** Função dir() ****
+Lista:
+{nomes_lista}
+
+String:
+{nomes_str}
+
+Local:
+{nomes_local}
+""")

e26a7a57a6c15370cd5611647f0540cf425d530b
Bugs <bferreira.slv@gmail.com>
2024-12-31 11:21:19 -0300

    code 36

---
+++ code/one-minute-036.py
@@ -0,0 +1,28 @@
+import math
+
+# valores
+valor_pi = math.pi
+valor_e = math.e
+valor_tau = math.tau
+
+# calculos
+potencia = math.pow(9,2)
+log = math.log2(9)
+raiz = math.sqrt(9)
+
+
+print(f""" **** Modulo Math ****
+π (pi):
+    {valor_pi}
+e (euler):
+    {valor_e}
+τ (tau):
+    {valor_tau}
+
+x elevado a y:
+    {potencia}
+Log2 de x:
+    {log}
+Raiz quadrada de x:
+    {raiz}
+""")
